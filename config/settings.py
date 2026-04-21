8e0efbb61668f867bd671f430f6f39aa6b1bf13a
kthr <kthr_tkr@outlook.jp>
2026-04-22 01:41:44 +0900

    修正

--- config/settings.py
+++ config/settings.py
@@ -173,5 +173,13 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "no-reply@example.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = "Carefrow <no-reply@carefrow.com>"
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
